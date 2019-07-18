9000461a9eaf0597ea317172ff9a4fa1c887a0ed
claudiaramirez <claudiaivonne_15@hotmail.com>
2019-07-18 17:20:33 -0500

    sign up form

--- src/Components/header/HeaderLogin.js
+++ src/Components/header/HeaderLogin.js
@@ -2,15 +2,23 @@ import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button';
 import logo from '../../assets/logo_white.png'; 
 import './header.css'; 
-
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 class HeaderLogin extends Component {
     render() {
         return (
-            <div>
-                    <img className="logo" src={logo}></img>
-                    <Button variant="primary">hola</Button>
-                
+            <div className='header-login'>
+                <Container>
+                    <Row>
+                        <Col xs={12}><img className="logo" src={logo}></img></Col>
+                    </Row>
+                    <Row>
+                        <Col xs={6}>Ingresar</Col>
+                        <Col xs={6}>Registrate</Col>
+                    </Row>
+                </Container>    
             </div>
         );
     }
