7cf66201f83504e460fcf9e5d71bc0f99d11eef4
claudiaramirez <claudiaivonne_15@hotmail.com>
2019-07-18 14:33:51 -0500

    components bascis

---
+++ src/Components/header/HeaderLogin.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class HeaderLogin extends Component {
+    render() {
+        return (
+            <div>
+                
+                    Hola soy navbar :3
+                
+            </div>
+        );
+    }
+}
+
+export default HeaderLogin;
