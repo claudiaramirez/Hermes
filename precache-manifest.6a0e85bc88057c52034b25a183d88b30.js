8fcb20c5a9d29288cd16e240e8ba34ab991d46dd
claudiaramirez <claudiaivonne_15@hotmail.com>
2019-07-18 19:02:32 -0500

    Updates

---
+++ precache-manifest.6a0e85bc88057c52034b25a183d88b30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37cb172d78bab069ee0c73d19eeae78f",
+    "url": "/Hermes/index.html"
+  },
+  {
+    "revision": "498f947d81f1f28398e1",
+    "url": "/Hermes/static/css/main.bb3db809.chunk.css"
+  },
+  {
+    "revision": "2da6ae34a7e15ad14f79",
+    "url": "/Hermes/static/js/2.065486ca.chunk.js"
+  },
+  {
+    "revision": "498f947d81f1f28398e1",
+    "url": "/Hermes/static/js/main.3f54a30b.chunk.js"
+  },
+  {
+    "revision": "ab6a91de7218b4e0b541",
+    "url": "/Hermes/static/js/runtime~main.686a1333.js"
+  }
+]);
