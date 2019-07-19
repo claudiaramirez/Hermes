c09501714bb75300b0bdbd7f2849811471565462
elenore29 <elenore.nava@gmail.com>
2019-07-19 00:24:37 -0500

    Bajando cambios

--- src/Views/signup/index.js
+++ src/Views/signup/index.js
@@ -2,17 +2,36 @@ import React, { Component } from 'react';
 import './signup.css'; 
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import Home from '../../Components/Components-Router/Home';
-
 
 class SignUp extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputValue: '',
+        }
+        this.handleInput = this.handleInput.bind(this); 
+    }
+
+ /*    handleInput = (event) => {
+        console.log(event);
+       /*  const valor = event.target.value; 
+        console.log(valor)
+        this.setState({
+            inputValue: valor
+        });  
+    } */
         render() {
+            console.log("nombre",this.state.inputValue)
         return (
             <div className="signup font-weight-bold">
                 <Form>
                 <Form.Group controlId="formBasicEmail">
                         <Form.Label>Nombre</Form.Label>
-                        <Form.Control className="input-signup" type="email" placeholder="Escribe tu nombre completo" />
+                        <Form.Control value={this.state.inputValue} onChange={(event)=>{
+                            this.setState({
+                                    inputValue: event.target.value
+                                })
+                            }} className="input-signup" type="email" placeholder="Escribe tu nombre completo" />
                         <Form.Text className="text-muted">
                         </Form.Text>
                     </Form.Group>
@@ -32,6 +51,7 @@ class SignUp extends Component {
                     </Form.Group>
 
                     <Button variant="primary" type="submit" className="btn-signup" onSubmit={this.SingUpEvent}>Registrate</Button>
+                    <Button variant="primary" type="submit" className="btn-signup" onClick={()=>console.log(this.state.inputValue)}>Registrate</Button>
                     </Form>
             </div>
         );
