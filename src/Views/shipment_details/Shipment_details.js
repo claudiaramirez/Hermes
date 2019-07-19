15637d7672a8fa7a5717570d9003bcacdf10fb25
unknown <j.dgez.p@gmail.com>
2019-07-19 09:26:08 -0500

    modificacion a navbar para ocultar, prueba

--- src/Views/shipment_details/Shipment_details.js
+++ src/Views/shipment_details/Shipment_details.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import Card from 'react-bootstrap/Card';
-import ListGroup from 'react-bootstrap/ListGroup';
 import './Shipment_details.css';
-import PackageDetails from './Package_details';
 import ShippingCompanyLogo from '../../assets/fedex_logo.png';
 import { detalle_guia } from "../../data/detalle_guia.js";
 import Badge from 'react-bootstrap/Badge';
@@ -23,23 +21,16 @@ class Shipment_details extends Component {
 
   render() {
 
-    const events = [
-      {ts: "2017-09-17T12:22:46.587Z", text: 'Logged in'},
-      {ts: "2017-09-17T12:21:46.587Z", text: 'Clicked Home Page'},
-      {ts: "2017-09-17T12:20:46.587Z", text: 'Edited Profile'},
-      {ts: "2017-09-16T12:22:46.587Z", text: 'Registred'},
-      {ts: "2017-09-16T12:21:46.587Z", text: 'Clicked Cart'},
-      {ts: "2017-09-16T12:20:46.587Z", text: 'Clicked Checkout'},
-    ];
-    
+
+
     return (
       <div className="card-format">
         <div>
           {
             this.state.listaGuias.map((value) => {
-              return value.id == "01guia" ? (
+              return value.id === "01guia" ? (
                 <div className="card-format">
-                  <Card style={{ width: '21rem' }}>
+                  <Card  style={{ width: '21rem' }}>
                     <Card.Body>
                       <Card.Img variant="top" className="rounded-circle img-size" src={ShippingCompanyLogo} />
                       <Card.Title><span>{value.tienda}</span></Card.Title>
@@ -58,7 +49,7 @@ class Shipment_details extends Component {
                            </Accordion.Toggle>
                         </Card.Header>
                         <Accordion.Collapse eventKey="0">
-                          <Card.Body><Timeline items={events} /></Card.Body>
+                          <Card.Body></Card.Body>
                         </Accordion.Collapse>
                       </Card>
                     </Accordion>
