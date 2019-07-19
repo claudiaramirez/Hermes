ea250870ace178c3f241f2bceb6999aeaa45035b
jaurinu <jaurinu@gmail.com>
2019-07-19 11:25:45 -0500

    Se modifica css lista de guia

--- src/Views/guideList/guideList.js
+++ src/Views/guideList/guideList.js
@@ -5,7 +5,6 @@ import './GuideList.css';
 import Tabs from 'react-bootstrap/Tabs';
 import Tab from 'react-bootstrap/Tab';
 import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
 import Icon_sig from '../../assets/icono_siguiente.png';
 
 import { Lista_de_guia } from  '../../data/Lista_de_guia';
@@ -28,6 +27,9 @@ class GuideList extends Component {
             <Card.Text>
               {`Guía: ${item.num_guia}`}
             </Card.Text>
+            <Card.Text>
+            {`Entrega estimada: ${item.created_at}`}
+            </Card.Text>
             <div><img id={item.id} src={Icon_sig} alt="icono" className="icon_format"></img></div>
           </Card.Body>
         </Card>
@@ -40,34 +42,40 @@ const transito = this.state.Lista_de_guia.map((item, i) => {
             <Card key={item.num_guia}>
             <Card.Body>
             <Card.Title>{item.tienda}</Card.Title>
-            <Card.Text>
+            <Card.Text >
             {`Guía: ${item.num_guia}`}
             </Card.Text>
-            <Button variant="primary" className="btn-signup">Detalles</Button>
+            <Card.Text>
+            {`Entrega estimada: ${item.created_at}`}
+            </Card.Text>
+            <div><img id={item.id} src={Icon_sig} alt="icono" className="icon_format"></img></div>
             </Card.Body>
             </Card>
              )}       
         });
 
-    const entregado = this.state.Lista_de_guia.map((item, i) => {
-        if ( item.estatus.estatus == "entregado") {
-            return(
-            <Card key={item.num_guia}>
-            <Card.Body className="card-format">
-            <Card.Title className="title-format">{item.tienda}</Card.Title>
-            <Card.Text className="title-format">
-            {`Guía: ${item.num_guia}`}
-            </Card.Text>
-            <Button variant="primary">Detalles</Button>
-            </Card.Body>
-            </Card>
-            )}       
-        });
+        const entregado = this.state.Lista_de_guia.map((item, i) => {
+          if ( item.estatus.estatus == "entregado") {
+              return(
+                  <Card key={item.num_guia}>
+                  <Card.Body>
+                  <Card.Title className="title_format">{item.tienda}</Card.Title>
+                  <Card.Text>
+                  {`Guía: ${item.num_guia}`}
+                  </Card.Text>
+                  <Card.Text>
+                  {`Entrega estimada: ${item.created_at}`}
+                  </Card.Text>
+                  <div><img id={item.id} src={Icon_sig} alt="icono" className="icon_format"></img></div>
+                  </Card.Body>
+                  </Card>
+                   )}       
+              });
 
 
         return (
             <div>
-            <Tabs defaultActiveKey="home" transition={false} id="noanim-tab-example">
+            <Tabs defaultActiveKey="camino" transition={false} id="noanim-tab-example">
             <Tab eventKey="camino" title="Camino" tabClassName='header_format'>
            {camino}
             </Tab>
