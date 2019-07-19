6c696ae86a0a5b8a620f7f564e9cbee0353246f4
unknown <j.dgez.p@gmail.com>
2019-07-19 01:24:17 -0500

    se crea json para presentacion de datos de envio

---
+++ src/Detalle_guia.js
@@ -0,0 +1,32 @@
+import { callExpression } from "@babel/types";
+import { cpus } from "os";
+
+{ detalle_guia: 
+   { 
+    id: "01guia",
+    direccion_origen: {
+        callExpressionnumero
+        colonia
+        cpusciudad
+        estado
+        tipo
+        num_guia
+
+    }
+    direccion_destino:{
+        callExpressionnumero
+        colonia
+        cpusciudad
+        estado
+        tipo
+        num_guia
+    }
+    historia:[]
+    fecha:
+    paquete:
+    imagen_paqueteria:
+    tienda:
+    estatus:{}
+}
+
+}
