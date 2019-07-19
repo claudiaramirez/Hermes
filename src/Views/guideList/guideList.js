5154a33a0782eb913ef61bdc692e8b2c785bb866
jaurinu <jaurinu@gmail.com>
2019-07-19 13:19:37 -0500

    Cambios css

--- src/Views/guideList/guideList.js
+++ src/Views/guideList/guideList.js
@@ -19,16 +19,18 @@ class GuideList extends Component {
    }
 
     render() {
+
+
         const camino = this.state.Lista_de_guia.map((item, i) => {
       if ( item.estatus.estatus === "camino") {
             return(
             <Card  key={item.num_guia}>
             <Card.Body>
-            <Card.Title>{item.tienda}</Card.Title>
-            <Card.Text>
+            <Card.Title>{item.username}</Card.Title>
+            <Card.Text className="text-card-format">
               {`Guía: ${item.num_guia}`}
-            </Card.Text>
-            <Card.Text>
+            </Card.Text >
+            <Card.Text className="text-card-format">
              {`Entrega estimada: ${item.created_at}`}
              </Card.Text>
             <div><img onClick={()=>this.props.history.push('/detalle')} id={item.id} src={Icon_sig} alt="icono" className="icon_format"></img></div>
@@ -42,11 +44,11 @@ const transito = this.state.Lista_de_guia.map((item, i) => {
         return(
                 <Card  key={item.num_guia}>
                 <Card.Body>
-                <Card.Title>{item.tienda}</Card.Title>
-                <Card.Text>
+                <Card.Title>{item.username}</Card.Title>
+                <Card.Text className="text-card-format">
                   {`Guía: ${item.num_guia}`}
                 </Card.Text>
-                <Card.Text>
+                <Card.Text className="text-card-format">
                  {`Entrega estimada: ${item.created_at}`}
                  </Card.Text>
                 <div><img onClick={()=>this.props.history.push('/detalle')} id={item.id} src={Icon_sig} alt="icono" className="icon_format"></img></div>
@@ -60,11 +62,11 @@ const transito = this.state.Lista_de_guia.map((item, i) => {
             return(
                     <Card  key={item.num_guia}>
                     <Card.Body>
-                    <Card.Title>{item.tienda}</Card.Title>
-                    <Card.Text>
+                    <Card.Title>{item.username}</Card.Title>
+                    <Card.Text className="text-card-format">
                       {`Guía: ${item.num_guia}`}
                     </Card.Text>
-                    <Card.Text>
+                    <Card.Text className="text-card-format">
                      {`Entrega estimada: ${item.created_at}`}
                      </Card.Text>
                     <div><img onClick={()=>this.props.history.push('/detalle')} id={item.id} src={Icon_sig} alt="icono" className="icon_format"></img></div>
